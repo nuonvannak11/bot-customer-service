1c4e993f0fe78d39909dc7fa0add4ef2ddd082b2
nuonvannak <nuonvannaksingle@gmail.com>
2026-01-08 07:23:54 +0700

    add new code

---
+++ frontend/src/controller/controller_user.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+class UserController {
+    public async login(req: Request, res: Response) {
+       
+    }
+
+    public async register(req: Request, res: Response) {
+        
+    }
+
+    public async verifyPhone(req: Request, res: Response) {
+      
+        try {
+            
+        } catch (error) {
+
+        }
+    }
+
+ 
+    public async googleLogin(req: Request, res: Response) {
+        try {
+            
+        } catch (err) {
+        }
+    }
+
+    
+}
+
+export default UserController;
